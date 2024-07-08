c6042a90f3e7ec19411847777a61762c0ecbe661
Keith Stanley <iamstan13y@gmail.com>
2024-07-08 23:38:40 +0200

    Added Models.

---
+++ WhatsAppBot.Alpha/Models/WhatsAppMessage.cs
@@ -0,0 +1,11 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace WhatsAppBot.Alpha.Models;
+
+public class WhatsAppMessage
+{
+    public string MessagingProduct { get; set; } = "whatsapp";
+    public string To { get; set; }
+    public string Type { get; set; } = "text";
+    public Text Text { get; set; }
+}
